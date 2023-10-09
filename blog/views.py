ddda6c26fffa2475c32530e2eaaa2a36e9ed1817
CYY1007 <younguk86@gmail.com>
2023-10-09 21:00:57 +0900

    :sparkles: Feat : Yolo 연동

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
 def post_list(request):
+    print("hi")
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
@@ -37,3 +40,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+class IntruderImage(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
